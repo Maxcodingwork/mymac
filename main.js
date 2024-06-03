4429c50bda07da8016691d9d09a9ec2d9c52dbee
Maxcodingwork <104746670+Maxcodingwork@users.noreply.github.com>
2024-06-03 11:34:03 +0800

    Update main.js

--- main.js
+++ main.js
@@ -3,8 +3,13 @@ allImages.forEach(function(img) {
     img.onclick = function () {
         let mySrc = img.getAttribute("src");
         let altSrc = img.getAttribute("data-alt-src");
+        let myAlt = img.getAttribute("alt");
+        let altAlt = img.getAttribute("data-alt-alt");
         img.setAttribute("src", altSrc);
         img.setAttribute("data-alt-src", mySrc);
+        img.setAttribute("alt", altAlt);
+        img.setAttribute("data-alt-alt", myAlt);
+        )
     };
 });
 
